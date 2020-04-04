3ac49428f7a8cce32c9cddb3a619c02e308722a0
kamr <kamalraj.gupthar@oracle.com>
2020-04-05 00:20:29 +0530

    Implemented draft version of get_volume_statistics() microservice

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
 
 app = Flask(__name__)
 
+def collect_volume_statistics():
+	volStat = {"name": "/u01",
+			   "size": 540,
+			   "foo": "bar"}
+
+	return volStat
 
 @app.route('/')
 def hello_world():
-	return 'Hello World!'
+	return 'Welcome to Get Volume Statistics Microservice app!'
 
+@app.route('/getVolumeStat')
+def get_volume_statistics():
+	return jsonify(collect_volume_statistics())
 
 if __name__ == '__main__':
-	app.run()
+	app.run(port=5000)
